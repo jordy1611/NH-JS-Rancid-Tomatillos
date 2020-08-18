493dc8fed62f1f52c8b35308ff07f1f4222d71b3
Jordy Shryock <jordy1611@gmail.com>
2020-08-18 15:07:06 -0700

    Create poster react class, display posters through app class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Posters from './Posters.js'
 import './App.css';
 
-function App() {
-  return (
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return(
     <main className="App">
       <header>
         <h1>Rotten Tomatillos</h1>
-        
+        <button>Login/Logout</button>
       </header>
+      <Posters />
     </main>
-  );
+  )};
 }
 
 export default App;
---
+++ src/Posters.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import './Posters.css';
+
+
+const Posters = (props) => {
+  return (
+    <h2>TEST</h2>
+  )
+}
+
+export default Posters
