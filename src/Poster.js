16d895d7b17788a8fc7e53f0d5996d351bc39f8e
Jordy Shryock <jordy1611@gmail.com>
2020-08-18 15:43:45 -0700

    Format posters display

--- src/Poster.js
+++ src/Poster.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import './Poster.css'
 
 const Poster = (props) => {
   const poster = props.poster
   console.log(poster)
   return (
     <figure className="poster">
-      <img src={poster.poster_path} alt={poster.title} />
       <figcaption>{poster.average_rating}</figcaption>
+      <img src={poster.poster_path} alt={poster.title} />
     </figure>
   )
 }
