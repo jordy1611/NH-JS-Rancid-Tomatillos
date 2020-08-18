bec67b2bd13a3e3941bf2ddabe8ea4bc614acd67
Jordy Shryock <jordy1611@gmail.com>
2020-08-18 15:34:00 -0700

    Set up unstyled poster display

--- src/Poster.js
+++ src/Poster.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Poster = (props) => {
-  return (<h1>POSTER</h1>)
+  const poster = props.poster
+  console.log(poster)
+  return (
+    <figure className="poster">
+      <img src={poster.poster_path} alt={poster.title} />
+      <figcaption>{poster.average_rating}</figcaption>
+    </figure>
+  )
 }
 
 export default Poster
