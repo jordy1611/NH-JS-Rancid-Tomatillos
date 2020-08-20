0711245f6dc91b047f2f54a242ec2a9e8b7f0d65
Nick Hart <nick.hart.des@gmail.com>
2020-08-20 17:41:32 -0600

    Add test for movie info display

--- src/MovieInfo/MovieInfo.test.js
+++ src/MovieInfo/MovieInfo.test.js
@@ -1,4 +1,32 @@
 import React from 'react';
-import MovieInfo from '../MovieInfo/MovieInfo.js'
-import { screen, fireEvent, render } from '@testing-library/react';
+import MovieInfo from '../MovieInfo/MovieInfo.js';
+import testMovie from './testMovie.js';
+import { screen, render } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
+describe('MovieInfo', () => {
+
+  it('should display all relevant movie information', () => {
+    render(<MovieInfo movie={testMovie}/>)
+
+    const backdrop = screen.getByAltText('Akira backdrop');
+    const heading = screen.getByRole('heading');
+    const releaseDate = screen.getByTestId('release date');
+    const overview = screen.getByTestId('overview');
+    const genres = screen.getByTestId('genres');
+    const budget = screen.getByTestId('budget');
+    const runtime = screen.getByTestId('runtime');
+    const tagline = screen.getByTestId('tagline');
+    const averageRating = screen.getByTestId('average rating');
+  
+    expect(backdrop).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+    expect(releaseDate).toBeInTheDocument();
+    expect(overview).toBeInTheDocument();
+    expect(genres).toBeInTheDocument();
+    expect(budget).toBeInTheDocument();
+    expect(runtime).toBeInTheDocument();
+    expect(tagline).toBeInTheDocument();
+    expect(averageRating).toBeInTheDocument();
+  })
+})
---
+++ src/MovieInfo/testMovie.js
@@ -0,0 +1,19 @@
+const testMovie = {
+  "id": 149,
+  "title": "Akira",
+  "poster_path": "https://image.tmdb.org/t/p/original//5KlRFKKSbyCiyYpZSS3A6G5bW0K.jpg",
+  "backdrop_path": "https://image.tmdb.org/t/p/original//qZ4NYuwME0j6QgJmIE6AZMgmCaj.jpg",
+  "release_date": "1988-07-16",
+  "overview": "A secret military project endangers Neo-Tokyo when it turns a biker gang member into a rampaging psychic psychopath that only two teenagers and a group of psychics can stop.",
+  "genres": [
+    "Science Fiction",
+    "Animation"
+  ],
+  "budget": 10000000,
+  "revenue": 0,
+  "runtime": 124,
+  "tagline": "Neo-Tokyo is about to E.X.P.L.O.D.E.",
+  "average_rating": 9
+}
+
+export default testMovie;
