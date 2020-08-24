9a26d7004dd640d54208bea7cb39e1d478483dd4
Nick Hart <nick.hart.des@gmail.com>
2020-08-24 17:05:56 -0600

    Add getLoginResponse to dataFetcher

--- src/dataFetcher.js
+++ src/dataFetcher.js
@@ -18,6 +18,21 @@ const dataFetcher = {
     const data = await response.json();
 
     return data.ratings; 
+  },
+
+  async getLoginResponse(credentials) {
+    const response = await fetch(
+      fetch("https://rancid-tomatillos.herokuapp.com/api/v2/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(credentials),
+      })
+    );
+    const data = response.json();
+
+    return data;
   }
 }
 
