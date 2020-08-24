775a29c37b40aa6c9412836e73217bbac85e88d8
Jordy Shryock <jordy1611@gmail.com>
2020-08-24 16:18:22 -0700

    Create and style user submit rating form

--- src/MovieInfo/MovieInfo.js
+++ src/MovieInfo/MovieInfo.js
@@ -3,6 +3,12 @@ import './MovieInfo.css'
 
 const MovieInfo = (props) => {
   const movie = props.movie;
+  const preventDefault = (e) => {
+    e.preventDefault()
+  }
+  const resetInput = (e) => {
+    e.target.value = ''
+  }
 
   return (
     <article className="movie-info">
@@ -16,7 +22,26 @@ const MovieInfo = (props) => {
       <p data-testid='budget'>Budget: {movie.budget}</p>
       <p data-testid='runtime'>Runtime: {movie.runtime} days</p>
       <p data-testid='tagline'>Tagline: {movie.tagline}</p>
-      <p data-testid='average rating'>Average Rating: {movie.average_rating}</p>
+      <p data-testid='average rating'>Average Rating: {movie.average_rating.toFixed(1)}</p>
+      <form className="user-rating-form">
+        <fieldset className="movie-fieldset">
+          <label htmlFor="rating-input">Your Rating:</label>
+          <input id="rating-input" list="numbers" min='0' max='10' onKeyDown={preventDefault} onClick={resetInput}></input>
+          <datalist id="numbers">
+            <option value="1"></option>
+            <option value="2"></option>
+            <option value="3"></option>
+            <option value="4"></option>
+            <option value="5"></option>
+            <option value="6"></option>
+            <option value="7"></option>
+            <option value="8"></option>
+            <option value="9"></option>
+            <option value="10"></option>
+          </datalist>
+          <button type="button">Submit</button>
+        </fieldset>
+      </form>
     </article>
   )
 }
