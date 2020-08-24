5a1651d057aeb909f3417d65c59972794a23e208
Nick Hart <nick.hart.des@gmail.com>
2020-08-24 16:13:10 -0600

    Add first acceptance test

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,14 +1,33 @@
+import MutationObserver from '@sheerun/mutationobserver-shim';
+window.MutationObserver = MutationObserver;
 import React from 'react';
 import App from './App';
-import Posters from '../Posters/Posters.js'
-import Header from '../Header/Header.js'
-import Login from '../Login/Login.js'
-import MovieInfo from '../MovieInfo/MovieInfo.js'
-import { screen, fireEvent, render } from '@testing-library/react';
+import Posters from '../Posters/Posters.js';
+import Header from '../Header/Header.js';
+import Login from '../Login/Login.js';
+import MovieInfo from '../MovieInfo/MovieInfo.js';
+import sampleMovies from '../sampleMovies.js';
+import { screen, fireEvent, render, waitFor } from '@testing-library/react';
+import dataFetcher from '../dataFetcher.js';
 import '@testing-library/jest-dom';
+jest.mock('../dataFetcher.js');
 
 describe('App', () => {
-  it('should exist', () => {
-    
+  it('display all movies when the page loads', async () => {
+    dataFetcher.getAllMovies.mockResolvedValueOnce(sampleMovies);
+
+    render(<App />);
+
+    const movie1 = await waitFor(() => screen.getByAltText('Greenland'));
+    const movie2 = await waitFor(() => screen.getByAltText('Archive'));
+    const movie3 = await waitFor(() => screen.getByAltText('Akira'));
+    const movie4 = await waitFor(() => screen.getByAltText('Friend of the World'));
+    const movie5 = await waitFor(() => screen.getByAltText('The Secret Garden'));
+
+    expect(movie1).toBeInTheDocument();
+    expect(movie2).toBeInTheDocument();
+    expect(movie3).toBeInTheDocument();
+    expect(movie4).toBeInTheDocument();
+    expect(movie5).toBeInTheDocument();
   })
 })
