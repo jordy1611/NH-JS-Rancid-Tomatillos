159ab86611dd74d636672ff735aaeab078f4f32a
Nick Hart <nick.hart.des@gmail.com>
2020-08-26 19:25:18 -0600

    Add Redirect on successful login

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, match } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Posters from '../Posters/Posters.js';
 import Header from '../Header/Header.js';
 import Login from '../Login/Login.js';
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import dataFetcher from '../dataFetcher';
+import { Redirect } from 'react-router-dom';
 import './Login.css';
 
 class Login extends Component {
@@ -8,7 +9,8 @@ class Login extends Component {
     this.state = {
       email: '',
       password: '',
-      error: ''
+      error: '',
+      loginOk: false
     };
   }
 
@@ -34,12 +36,17 @@ class Login extends Component {
       this.props.displayHomePage();
       this.props.updateCurrentUser(loginResponse.user);
       this.props.displayUserRatings();
+      this.setState({loginOk: true});
     } else {
       this.setState({error: 'uh oh'});
     }
   }
 
   render() {
+    if (this.state.loginOk === true) {
+      return <Redirect to='/'/>
+    }
+
     return (
       <form>
         <fieldset className="login-fieldset">
