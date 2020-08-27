cf4ed3d0a6f994e98dd7f1acbed3cf9fcdd2a5d8
Jordy Shryock <jordy1611@gmail.com>
2020-08-27 13:36:58 -0700

    Style user ratings on home page

--- src/Poster/Poster.js
+++ src/Poster/Poster.js
@@ -7,11 +7,12 @@ const Poster = (props) => {
   const averageRating = props.poster.average_rating.toFixed(1);
   const userRating = props.userRating;
 
+  const rating = (userRating) ? `${averageRating}, ${userRating.rating}` : `${averageRating}`
+
   return (
     <Link to={`/movies/${poster.id}`}>
       <figure className="poster" onClick={props.setMovieView}>
-        <figcaption id={poster.id}>{averageRating}</figcaption>
-        {userRating && <p>{userRating.rating}</p>}
+        <figcaption id={poster.id}>{rating}</figcaption>
         <img src={poster.poster_path} alt={poster.title} id={poster.id} />
       </figure>
     </Link>
