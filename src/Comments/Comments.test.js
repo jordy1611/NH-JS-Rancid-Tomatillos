0eb8a52745c76f9e9066b4898c566c977cddefea
Nick Hart <nick.hart.des@gmail.com>
2020-08-28 12:00:37 -0600

    Set up first test

---
+++ src/Comments/Comments.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Comments from './Comments';
+import { screen, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Comments', () => {
+  it('should render without crashing', () => {
+    render(<Comments />);
+  })
+})
